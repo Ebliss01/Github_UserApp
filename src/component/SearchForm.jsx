e4ebed759a7069afecf0b662dd4204e5ff07d854
Oyiborhoro Benedicta <ladyfsquare@yahoo.com>
2023-10-16 13:14:41 +0100

    Module_Updated

--- src/component/SearchForm.jsx
+++ src/component/SearchForm.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { CgSearch } from 'react-icons/cg';
+import { ThemeContext } from './ContextTheme';
+
 
 const SearchForm = () => {
+const {darkTheme} = useContext(ThemeContext)
+
   return (
-    <div className='d-flex justify-content-between align-items-center w-50 form-wrapper py-3 px-2 rounded-10'>
+    
+    <div className={!darkTheme ? 'form-wrapper px-3 py-2 rounded':'form-wrapper dark px-3 py-2 rounded'}>
         <div className='d-flex gap-2 align-items-center w-100'>
         <CgSearch color='#0079FF' size='24px' />
-        <input type='text' placeholder='Search Github Username...' className='form-control boder border-o'/>
+        <input type='text' placeholder='Search Github Username...' className='form-control border border-0'/>
         </div>
       <button className='btn btn-primary'>Search</button>
     </div>
